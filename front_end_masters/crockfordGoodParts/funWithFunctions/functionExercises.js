551aa361dfff655db537cc7c53428c894a5c5085
Mo Amin <m.alamin92@gmail.com>
2016-08-03 21:55:00 -0400

    challenge 3 complete

--- front_end_masters/crockfordGoodParts/funWithFunctions/functionExercises.js
+++ front_end_masters/crockfordGoodParts/funWithFunctions/functionExercises.js
@@ -42,3 +42,56 @@ var curry = function(func, firstArg) {
 //console.log(inc(2));
 //var inc = curry(add, 1);
 //console.log(inc(2));
+
+var twice = function(binaryFunc) {
+  return function unaryFunc(argOne) {
+    return binaryFunc(argOne, argOne);
+  };
+};
+
+var doub = twice(add);
+//console.log(doub(11));
+var square = twice(mul);
+//console.log(square(11));
+
+var reverse = function(funcToRev) {
+  return function(argOne, argTwo) {
+    return funcToRev(argTwo, argOne);
+  };
+};
+
+//var bus = reverse(sub);
+//console.log(bus(3,2));
+
+var composeu = function(funcOne, funcTwo) {
+  return function(argOne) {
+    return funcTwo(funcOne(argOne));
+  };
+};
+
+//console.log(composeu(doub, square)(10));
+
+var composeb = function(funcOne, funcTwo) {
+  return function (argOne, argTwo, argThree) {
+    return funcTwo(funcOne(argOne, argTwo), argThree);
+  };
+};
+
+//console.log(composeb(add, mul)(2,3,7));
+
+var limit = function(binFunc, times) {
+  var numTimesRun = 0;
+  return function(argOne, argTwo) {
+    numTimesRun += 1;
+    if(numTimesRun <= times) { 
+      return binFunc(argOne, argTwo);
+    }
+    return undefined;
+  };
+};
+
+//var addLtd = limit(add, 2);
+//console.log(addLtd(3,4));
+//console.log(addLtd(3,5));
+//console.log(addLtd(3,4));
+//console.log(addLtd(3,5));
