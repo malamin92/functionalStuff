cf039c159b39e9e406c831c4361d258571a2025c
Mo Amin <m.alamin92@gmail.com>
2016-08-17 19:08:36 -0400

    prototypical delagation

--- front_end_masters/advanced-pure-dom/exercises/JS/my_js.js
+++ front_end_masters/advanced-pure-dom/exercises/JS/my_js.js
@@ -8,14 +8,32 @@ DOT = function(obj, prop){
 
 // 05 Context
 DOTCALL = function(obj, prop, args){
+  var func = DOT(obj, prop);
 
+  if( func ){
+    return func.apply( obj, args );
+  }
 }
 
 // 06 Prototypes
 NEW = function(constructor, args){
-
+  // what is the new keyword doing??
+  // 1. creating a new object.
+  var o = {};
+  // 2. point the new objects __proto__ to our constructors prototype object
+  o.__proto__ = constructor.prototype;
+  // 3. invoke the constructor with our new object as context
+  constructor.apply(o, args);
+  // 4. returning the new object we just created.
+  return o;
 }
 
 INSTANCEOF = function(obj, constructor){
-
+  if(obj.__proto__ === constructor.prototype){
+    return true;
+  }
+  else if(obj.__proto__){
+    return INSTANCEOF(obj.__proto__, constructor);
+  }
+  return false;
 }
